f0bc41ee1ab14d01d3c9fa71d63128f3b82e85d1
danjelous <daniel.brunner@live,at>
2021-11-16 20:57:28 +0100

    Utilize new card partial

--- vite-handlebars/vite.config.js
+++ vite-handlebars/vite.config.js
@@ -1,5 +1,11 @@
+// vite.config.js
+import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
-  plugins: [handlebars()],
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'partials'),
+    }),
+  ],
 };
