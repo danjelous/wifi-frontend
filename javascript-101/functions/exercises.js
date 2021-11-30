12e18adf15800ff13d0e88ef71ffe32285ad927c
danjelous <daniel.brunner@live,at>
2021-11-30 20:24:03 +0100

    Add functions exercise 4

--- javascript-101/functions/exercises.js
+++ javascript-101/functions/exercises.js
@@ -5,7 +5,7 @@
 
 function isValidPassword(password, username) {
   // Kriterium 0: Check ob beides strings sind
-  if(typeof password !== "string" || typeof username !== "string") {
+  if (typeof password !== "string" || typeof username !== "string") {
     // Mind. ein Argument kein string --> return false
     console.log(`password oder username kein string!`)
     return false;
@@ -15,7 +15,7 @@ function isValidPassword(password, username) {
 
   // Kriterium 1: Passwort muss mindestens 8 Zeichen lang sein
   // if(!password.length >= 8) { // Variante A, eher weniger 
-  if(password.length <= 8) { // Variante B, besser
+  if (password.length <= 8) { // Variante B, besser
     // Passt nicht, zu kurz
     console.log(`Passwort zu kurz! Mindestens 8 Zeichen, bekam aber nur ${password.length}`)
     return false;
@@ -36,7 +36,7 @@ function isValidPassword(password, username) {
 
   // Kriterium 3: Der Username darf nicht im Passwort enthalten sein
   // if(password.includes(username)) {   // Variante 1
-  if(password.indexOf(username) > -1) {  // Variante 2
+  if (password.indexOf(username) > -1) {  // Variante 2
     console.log(`Der username darf nicht im password enthalten sein!`)
     return false;
   }
@@ -45,11 +45,34 @@ function isValidPassword(password, username) {
   return true;
 }
 
-let validPassword;
-// validPassword = isValidPassword("8193jak!23", "dogLover"); // Erwartet: true, Ist: true
-// validPassword = isValidPassword("123dogLover123!");  // Erwartet: false; Ist: false
-// validPassword = isValidPassword("8193   jak!23");       // Erwartet: false; Ist: false
-// validPassword = isValidPassword("abc", "mom");          // Erwartet: false; Ist: false
-validPassword = isValidPassword("abcabcabc", "cab");       // Erwartet: false; Ist: false
+// let res;
+// res = isValidPassword("8193jak!23", "dogLover"); // Erwartet: true, Ist: true
+// res = isValidPassword("123dogLover123!");  // Erwartet: false; Ist: false
+// res = isValidPassword("8193   jak!23");       // Erwartet: false; Ist: false
+// res = isValidPassword("abc", "mom");          // Erwartet: false; Ist: false
+// res = isValidPassword("abcabcabc", "cab");       // Erwartet: false; Ist: false
 
-console.log(validPassword)
+// console.log(res)
+
+
+/** ----------------
+ * ---- Übung 2
+ * ----------------- */
+function isPangram(str) {
+  const charsToCheck = "abcdefghijklmnopqrstuvwxyz";
+
+  // Lowercase input 
+  const lc = str.toLowerCase();
+
+  for (let i = 0; i < charsToCheck.length; i++) {
+    if (!lc.includes(charsToCheck[i])) return false;
+  }
+
+  return true;
+}
+
+// let res;
+// res = isPangram("Vogel Quax zwickt Johnys Pferd Bim"); // true
+// res = isPangram("Franz jagt im verwahrlosten Taxi quer durch Bayern"); // false
+// res = isPangram("the quick brown fox jumps over the lazy dog"); // true
+// console.log(res)
